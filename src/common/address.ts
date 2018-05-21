866146d697eb642dca36ee9051c980eba678aa26
Niall Moore <niall@Nialls-iMac.local>
2018-05-21 09:36:34 +0900

    refactor of address class

--- src/common/address.ts
+++ src/common/address.ts
@@ -1,22 +1,27 @@
-import * as Base58 from 'base-58'
-import * as proto from '../serialization/proto'
-import { Hash } from '../utils/hash'
+import * as Base58 from "base-58"
+import { getLogger } from "log4js"
+import * as proto from "../serialization/proto"
+import { Hash } from "../util/hash"
+
+const logger = getLogger("Address")
 
 function checkSum(arr: Uint8Array): string {
     const hash = Hash.hash(arr)
     let str = Base58.encode(hash)
-    str = str.slice(0, 4)
-    return str
+    return str.slice(0, 4)
 }
 
 function toUint8Array(address: (string | Uint8Array)) {
     if (typeof address == 'string') {
+        if (address.charAt(0) !== "H") {
+            throw new Error(`Address is invalid. Expected address to start with 'H'`)
+        }
         const check = address.slice(-4)
         address = address.slice(1, -4)
         const out: Uint8Array = Base58.decode(address)
         const expectedChecksum = checkSum(out)
         if (expectedChecksum != check) {
-            throw new Error(`Address hash invalid checksum '${check}' epected '${expectedChecksum}'`)
+            throw new Error(`Address hash invalid checksum '${check}' expected '${expectedChecksum}'`)
         }
         return out
     }
@@ -24,35 +29,34 @@ function toUint8Array(address: (string | Uint8Array)) {
 }
 
 export class Address extends Uint8Array {
+    public static isAddress(address: string): boolean {
+        try {
+            toUint8Array(address)
+            return true
+        } catch (e) {
+            return false
+        }
+    }
+
     constructor(address: string | Uint8Array | number) {
         typeof address == 'number' ? super(address) : super(toUint8Array(address)) // Need to allow for super constructor for number due to extension of Uint8Array
     }
 
     public toString(): string {
-        let str = ''
         try {
-            str = Base58.encode(this)
-            str += checkSum(this)
-            str = 'H' + str
-        } catch (err) {
-            console.log(err)
+            return "H" + Base58.encode(this) + checkSum(this)
+        } catch (e) {
+            logger.error(e)
         }
-
-        return str
     }
 
     public equals(address: Address): boolean {
-        if (this.length != address.length) { return false }
+        if (this.length !== address.length) { return false }
         for (let i = 0; i < address.length; i++) {
-            if (this[i] != address[i]) {
+            if (this[i] !== address[i]) {
                 return false
             }
         }
         return true
-    }
-
-    public static decode(string: string): Address {
-        string = string.slice(1, string.length - 4)
-        return new Address(Base58.decode(string))
     }
 }
